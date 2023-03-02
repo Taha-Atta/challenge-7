d5fd0f3578a2f14aa8f8110c1bcd7976f64519bf
taha <taha.marey100@gmail.com>
2023-03-02 12:13:04 +0300

    upload my project

---
+++ mains.js
@@ -0,0 +1,11 @@
+let phone = document.querySelector(".phone");
+let contaner = document.querySelector(".container")
+
+
+function phones(imgg) {
+    phone.src = imgg
+}
+
+function colors(color) {
+    contaner.style.background = color;
+}
